cb3d5fd687dcd6ab82cf0d498b1f887259b5f5e0
Farid Asadpour <84594007+yani5752000@users.noreply.github.com>
2023-08-11 16:50:17 -0400

    axios

--- expressServer/server.js
+++ expressServer/server.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+const messagesRouter = require("./routes/messages")
+
+app.use("/messages", messagesRouter);
 
 app.get("/", (req, res) => {
     res.send("good day")
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+  const getMessages = () => {
+    axios.get("http://localhost:8080/messages")
+      .then(result => {
+        console.log(result)
+      })
+      .catch(error => console.log(error))
+  };
+
+  const createMessage = () => {
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
